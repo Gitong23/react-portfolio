f7d2f86aa956a441a513dfe7f6f5dbf7e0c7a413
Karn <gitong23@gmail.com>
2024-04-08 00:50:15 +0700

    cicd clound run

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Myl First Deploy React count is {count}
+          Deploy React Version 1 count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
